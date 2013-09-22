1278dbf052c14b74ddfd605ac99b6527a1dc081f
Karl Bode <kpbode@gmail.com>
2013-09-22 08:07:03 +0200

    Initial Commit

---
+++ BreMensa/KPBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  KPBAppDelegate.h
+//  BreMensa
+//
+//  Created by Karl on 9/22/13.
+//  Copyright (c) 2013 Karl Bode. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KPBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BreMensa/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BreMensa
+//
+//  Created by Karl on 9/22/13.
+//  Copyright (c) 2013 Karl Bode. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "KPBAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([KPBAppDelegate class]));
+    }
+}
